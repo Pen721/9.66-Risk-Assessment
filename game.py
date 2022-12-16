212db3725b7660db112fcaf2672a72edf986452b
Caroline Cunningham <ccunning@mit.edu>
2022-12-15 22:39:16 -0500

    added text

--- game.py
+++ game.py
@@ -35,15 +35,16 @@ else:
     raise Exception("no distribution found ;-;???")
 
 B = balloons.getBallons()
+print(B)
 
 # Game Part
 
 pygame.init()
-SCREEN_HEIGHT = 400
-SCREEN_WIDTH = 300
+SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 600
 BALLOON_SIZE = 20
 DISPLAYSURF = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
-pygame.display.set_caption('Hello World!')
+pygame.display.set_caption('Balloon Game!')
  
 total_score = 0
 currBalloonIdx = 0
@@ -53,8 +54,22 @@ curr_pumps = 0 # aka player score
 lastKeyPressed = 0
 timeDelay = 2000 # wait 0.2 seconds between key presses
 
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+# https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
+
 while currBalloonIdx < numberBalloons: # main game loop
     max_pumps = B[currBalloonIdx]
+
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    currScoreTxt = font.render('This Round: ' + str(curr_pumps), True, RED, WHITE)
+    currScoreRect = currScoreTxt.get_rect()
+    totalScoreTxt = font.render('Points Earned: ' + str(total_score), True, BLUE, WHITE)
+    totalScoreRect = totalScoreTxt.get_rect()
+    totalScoreRect.center = (2 * SCREEN_WIDTH / 3, SCREEN_HEIGHT / 50)
+
     for event in pygame.event.get():
         if event.type == QUIT: 
             pygame.quit()
@@ -62,19 +77,24 @@ while currBalloonIdx < numberBalloons: # main game loop
             sys.exit()
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT: # INC BALLOON SIZE
-            print("MAX PUMPS")
-            print(max_pumps)
             # and pygame.time.get_ticks() - lastKeyPressed > timeDelay
             lastKeyPressed = pygame.time.get_ticks()
             curr_pumps += 1
 
-            if curr_pumps == max_pumps: # BALLOON POPS 
-                curr_pumps = 0
-                BALLOON_SIZE += 5
+            print("MAX PUMPS")
+            print(max_pumps)
+            print("CURR BALLOON")
+            print(currBalloonIdx)
+            print("CURR PUMPS")
+            print(curr_pumps)
 
-            else: # PUMP BALLOON 
+            if curr_pumps == max_pumps: # BALLOON POPS 
+                curr_pumps = 0 # reset current score
                 BALLOON_SIZE = 20 # reset to initial size
                 currBalloonIdx+=1
+
+            else: # PUMP BALLOON 
+                BALLOON_SIZE += 5
 
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP: # NEXT BALLOON
             lastKeyPressed = pygame.time.get_ticks()
@@ -83,7 +103,13 @@ while currBalloonIdx < numberBalloons: # main game loop
             BALLOON_SIZE = 20 # reset to initial size
             currBalloonIdx+=1
 
-        DISPLAYSURF.fill((255, 255, 255))  # white background
-        pygame.draw.circle(DISPLAYSURF, (0, 0, 255), (SCREEN_HEIGHT/2, SCREEN_WIDTH/2), BALLOON_SIZE)
+        DISPLAYSURF.fill(WHITE)  # white background
+        DISPLAYSURF.blit(currScoreTxt, currScoreRect)
+        DISPLAYSURF.blit(totalScoreTxt, totalScoreRect)
+
+        if currBalloonIdx % 2 == 0: # have color alternate between conseq. balloons
+            pygame.draw.circle(DISPLAYSURF, BLUE, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2), BALLOON_SIZE)
+        else:
+            pygame.draw.circle(DISPLAYSURF, RED, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2), BALLOON_SIZE)
 
         pygame.display.update()
